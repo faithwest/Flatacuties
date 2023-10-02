15a8cceb5a13e4c01e25a8750f6da4e77c25d4f5
faithwest <faithchepkoech425@gmail.com>
2023-10-02 22:32:28 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-// Your code here
+characterBars.forEach((characterBar) => {
+    const name = characterBar.querySelector("h4").textContent;
+    const paragraph = document.createElement("p");
+    paragraph.textContent = `Name: ${name}`;
+    characterBar.appendChild(paragraph);
+});
